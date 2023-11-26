6a0ba6848de526b143a107bab7907fee3d9c177c
jennyngo1925 <jennyngo1925@gmail.com>
2023-11-26 15:53:45 -0500

    nav bar

--- app/src/main/java/edu/uga/cs/shoppinglist/LoginActivity.java
+++ app/src/main/java/edu/uga/cs/shoppinglist/LoginActivity.java
@@ -58,7 +58,7 @@ public class LoginActivity extends AppCompatActivity {
                                     Toast.makeText(LoginActivity.this, "Login successful.",
                                             Toast.LENGTH_SHORT).show();
 
-                                    Intent intent = new Intent(getApplicationContext(), ShoppingList.class);
+                                    Intent intent = new Intent(getApplicationContext(), SplashPage.class);
                                     startActivity(intent);
 
                                 } else {
--- app/src/main/java/edu/uga/cs/shoppinglist/ShoppingList.java
+++ app/src/main/java/edu/uga/cs/shoppinglist/ShoppingList.java
@@ -1,138 +1,146 @@
-package edu.uga.cs.shoppinglist;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class ShoppingList
-        extends AppCompatActivity
-        implements AddItemDialogFragment.AddListItemDialogListener {
-//        EditJobLeadDialogFragment.EditJobLeadDialogListener {
-
-
-    private RecyclerView recyclerView;
-    private ShoppingListRecyclerAdapter recyclerAdapter;
-
-    private List<ListItem> itemsList;
-
-    private FirebaseDatabase database;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shopping_list);
-        recyclerView = findViewById(R.id.recyclerView);
-
-        Button addItem = findViewById(R.id.addItem);
-        addItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DialogFragment newFragment = new AddItemDialogFragment();
-                newFragment.show(getSupportFragmentManager(), null);
-            }
-        });
-
-        // initialize the Job Lead list
-        itemsList = new ArrayList<>();
-
-        // use a linear layout manager for the recycler view
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        // the recycler adapter with job leads is empty at first; it will be updated later
-        recyclerAdapter = new ShoppingListRecyclerAdapter( itemsList, ShoppingList.this );
-        recyclerView.setAdapter( recyclerAdapter );
-
-        // get a Firebase DB instance reference
-        database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("ShoppingList");
-
-        // Set up a listener (event handler) to receive a value for the database reference.
-        // This type of listener is called by Firebase once by immediately executing its onDataChange method
-        // and then each time the value at Firebase changes.
-        //
-        // This listener will be invoked asynchronously, hence no need for an AsyncTask class, as in the previous apps
-        // to maintain job leads.
-        myRef.addValueEventListener( new ValueEventListener() {
-
-            @Override
-            public void onDataChange( @NonNull DataSnapshot snapshot ) {
-                // Once we have a DataSnapshot object, we need to iterate over the elements and place them on our job lead list.
-                itemsList.clear(); // clear the current content; this is inefficient!
-                for( DataSnapshot postSnapshot: snapshot.getChildren() ) {
-                    ListItem jobLead = postSnapshot.getValue(ListItem.class);
-                    jobLead.setKey( postSnapshot.getKey() );
-                    itemsList.add( jobLead );
-                }
-
-                recyclerAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled( @NonNull DatabaseError databaseError ) {
-                System.out.println( "ValueEventListener: reading failed: " + databaseError.getMessage() );
-            }
-        } );
-    }
-
-    public void addListItem(ListItem listItem) {
-        // add the new job lead
-        // Add a new element (JobLead) to the list of job leads in Firebase.
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("ShoppingList");
-
-        // First, a call to push() appends a new node to the existing list (one is created
-        // if this is done for the first time).  Then, we set the value in the newly created
-        // list node to store the new job lead.
-        // This listener will be invoked asynchronously, as no need for an AsyncTask, as in
-        // the previous apps to maintain job leads.
-        myRef.push().setValue(listItem).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-
-                // Reposition the RecyclerView to show the JobLead most recently added (as the last item on the list).
-                // Use of the post method is needed to wait until the RecyclerView is rendered, and only then
-                // reposition the item into view (show the last item on the list).
-                // the post method adds the argument (Runnable) to the message queue to be executed
-                // by Android on the main UI thread.  It will be done *after* the setAdapter call
-                // updates the list items, so the repositioning to the last item will take place
-                // on the complete list of items.
-                recyclerView.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        recyclerView.smoothScrollToPosition(itemsList.size() - 1);
-                    }
-                });
-
-                // Show a quick confirmation
-                Toast.makeText(getApplicationContext(), "List item created for " + listItem.getItem(), Toast.LENGTH_SHORT).show();
-
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getApplicationContext(), "Failed to create a Job lead for " + listItem.getItem(), Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-}
+//package edu.uga.cs.shoppinglist;
+//
+//import androidx.annotation.NonNull;
+//import androidx.appcompat.app.ActionBarDrawerToggle;
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.widget.Toolbar;
+//import androidx.drawerlayout.widget.DrawerLayout;
+//import androidx.fragment.app.DialogFragment;
+//import androidx.fragment.app.Fragment;
+//import androidx.fragment.app.FragmentManager;
+//import androidx.recyclerview.widget.LinearLayoutManager;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.view.MenuItem;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.Toast;
+//
+//import com.google.android.gms.tasks.OnFailureListener;
+//import com.google.android.gms.tasks.OnSuccessListener;
+//import com.google.android.material.floatingactionbutton.FloatingActionButton;
+//import com.google.android.material.navigation.NavigationView;
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.ValueEventListener;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class ShoppingList
+//        extends AppCompatActivity
+//        implements AddItemDialogFragment.AddListItemDialogListener {
+////        EditJobLeadDialogFragment.EditJobLeadDialogListener {
+//
+//
+//    public static final String TAG = "ShoppingList";
+//
+//    private RecyclerView recyclerView;
+//    private ShoppingListRecyclerAdapter recyclerAdapter;
+//
+//    private List<ListItem> itemsList;
+//
+//    private FirebaseDatabase database;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_shopping_list);
+//        recyclerView = findViewById(R.id.recyclerView);
+//        Button addItem = findViewById(R.id.addItem);
+//        addItem.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                DialogFragment newFragment = new AddItemDialogFragment();
+//                newFragment.show(getSupportFragmentManager(), null);
+//            }
+//        });
+//
+//        // initialize the Job Lead list
+//        itemsList = new ArrayList<>();
+//
+//        // use a linear layout manager for the recycler view
+//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+//        recyclerView.setLayoutManager(layoutManager);
+//
+//        // the recycler adapter with job leads is empty at first; it will be updated later
+//        recyclerAdapter = new ShoppingListRecyclerAdapter( itemsList, ShoppingList.this );
+//        recyclerView.setAdapter( recyclerAdapter );
+//
+//        // get a Firebase DB instance reference
+//        database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("ShoppingList");
+//
+//        // Set up a listener (event handler) to receive a value for the database reference.
+//        // This type of listener is called by Firebase once by immediately executing its onDataChange method
+//        // and then each time the value at Firebase changes.
+//        //
+//        // This listener will be invoked asynchronously, hence no need for an AsyncTask class, as in the previous apps
+//        // to maintain job leads.
+//        myRef.addValueEventListener( new ValueEventListener() {
+//
+//            @Override
+//            public void onDataChange( @NonNull DataSnapshot snapshot ) {
+//                // Once we have a DataSnapshot object, we need to iterate over the elements and place them on our job lead list.
+//                itemsList.clear(); // clear the current content; this is inefficient!
+//                for( DataSnapshot postSnapshot: snapshot.getChildren() ) {
+//                    ListItem jobLead = postSnapshot.getValue(ListItem.class);
+//                    jobLead.setKey( postSnapshot.getKey() );
+//                    itemsList.add( jobLead );
+//                }
+//
+//                recyclerAdapter.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onCancelled( @NonNull DatabaseError databaseError ) {
+//                System.out.println( "ValueEventListener: reading failed: " + databaseError.getMessage() );
+//            }
+//        } );
+//    }
+//
+//    public void addListItem(ListItem listItem) {
+//        // add the new job lead
+//        // Add a new element (JobLead) to the list of job leads in Firebase.
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("ShoppingList");
+//
+//        // First, a call to push() appends a new node to the existing list (one is created
+//        // if this is done for the first time).  Then, we set the value in the newly created
+//        // list node to store the new job lead.
+//        // This listener will be invoked asynchronously, as no need for an AsyncTask, as in
+//        // the previous apps to maintain job leads.
+//        myRef.push().setValue(listItem).addOnSuccessListener(new OnSuccessListener<Void>() {
+//            @Override
+//            public void onSuccess(Void aVoid) {
+//
+//                // Reposition the RecyclerView to show the JobLead most recently added (as the last item on the list).
+//                // Use of the post method is needed to wait until the RecyclerView is rendered, and only then
+//                // reposition the item into view (show the last item on the list).
+//                // the post method adds the argument (Runnable) to the message queue to be executed
+//                // by Android on the main UI thread.  It will be done *after* the setAdapter call
+//                // updates the list items, so the repositioning to the last item will take place
+//                // on the complete list of items.
+//                recyclerView.post(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        recyclerView.smoothScrollToPosition(itemsList.size() - 1);
+//                    }
+//                });
+//
+//                // Show a quick confirmation
+//                Toast.makeText(getApplicationContext(), "List item created for " + listItem.getItem(), Toast.LENGTH_SHORT).show();
+//
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Toast.makeText(getApplicationContext(), "Failed to create a Job lead for " + listItem.getItem(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//    }
+//}
